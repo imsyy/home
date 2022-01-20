6e6fa9d7e4eab8f710fb9e53c101c27d56876bb5
底层用户 <ytqz104ban@126.com>
2022-01-20 17:36:09 +0800

    up music-list

--- js/music.js
+++ js/music.js
@@ -24,14 +24,6 @@ const ap = new APlayer({
             theme: "#0057a7"
         },
         {
-            name: "花海",
-            artist: "周杰伦",
-            url: "https://drive.imsyy.top/api?path=%2F%E9%9F%B3%E4%B9%90%2F%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8%2F%E5%91%A8%E6%9D%B0%E4%BC%A6-%E8%8A%B1%E6%B5%B7.mp3&raw=true",
-            cover: "https://y.qq.com/music/photo_new/T002R300x300M000002Neh8l0uciQZ_1.jpg?max_age=2592000",
-            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E5%91%A8%E6%9D%B0%E4%BC%A6-%E8%8A%B1%E6%B5%B7.lrc",
-            theme: "#954a17"
-        },
-        {
             name: "发如雪",
             artist: "周杰伦",
             url: "https://drive.imsyy.top/api?path=%2F%E9%9F%B3%E4%B9%90%2F%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8%2F%E5%91%A8%E6%9D%B0%E4%BC%A6-%E5%8F%91%E5%A6%82%E9%9B%AA.mp3&raw=true",
@@ -118,14 +110,6 @@ const ap = new APlayer({
             cover: "https://y.gtimg.cn/music/photo_new/T002R300x300M000000bviBl4FjTpO_1.jpg?max_age=2592000",
             lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E5%91%A8%E6%9D%B0%E4%BC%A6-%E7%83%9F%E8%8A%B1%E6%98%93%E5%86%B7.lrc",
             theme: "#86b5bb"
-        },
-        {
-            name: "给我一首歌的时间",
-            artist: "周杰伦",
-            url: "https://drive.imsyy.top/api?path=%2F%E9%9F%B3%E4%B9%90%2F%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8%2F%E5%91%A8%E6%9D%B0%E4%BC%A6-%E7%BB%99%E6%88%91%E4%B8%80%E9%A6%96%E6%AD%8C%E7%9A%84%E6%97%B6%E9%97%B4.mp3&raw=true",
-            cover: "https://y.gtimg.cn/music/photo_new/T002R300x300M000002Neh8l0uciQZ_1.jpg?max_age=2592000",
-            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E5%91%A8%E6%9D%B0%E4%BC%A6-%E7%BB%99%E6%88%91%E4%B8%80%E9%A6%96%E6%AD%8C%E7%9A%84%E6%97%B6%E9%97%B4.lrc",
-            theme: "#e3ae55"
         },
         {
             name: "霍元甲",
@@ -288,6 +272,86 @@ const ap = new APlayer({
             theme: "#d0e8f2"
         },
         {
+            name: "叹服",
+            artist: "许嵩",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/%E8%AE%B8%E5%B5%A9-%E5%8F%B9%E6%9C%8D.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M000002CJON012PxwU_1.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E8%AE%B8%E5%B5%A9-%E5%8F%B9%E6%9C%8D.lrc",
+            theme: "#def3fc"
+        },
+        {
+            name: "幻听",
+            artist: "许嵩",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/%E8%AE%B8%E5%B5%A9-%E5%B9%BB%E5%90%AC.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M000004RbL3b0BDIe2_1.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E8%AE%B8%E5%B5%A9-%E5%B9%BB%E5%90%AC.lrc",
+            theme: "#bf1c21"
+        },
+        {
+            name: "庐州月",
+            artist: "许嵩",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/%E8%AE%B8%E5%B5%A9-%E5%BA%90%E5%B7%9E%E6%9C%88.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M000002CJON012PxwU_1.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E8%AE%B8%E5%B5%A9-%E5%BA%90%E5%B7%9E%E6%9C%88.lrc",
+            theme: "#def3fc"
+        },
+        {
+            name: "断桥残雪",
+            artist: "许嵩",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/%E8%AE%B8%E5%B5%A9-%E6%96%AD%E6%A1%A5%E6%AE%8B%E9%9B%AA.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M000001jmC6x1RMfh0_2.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E8%AE%B8%E5%B5%A9-%E6%96%AD%E6%A1%A5%E6%AE%8B%E9%9B%AA.lrc",
+            theme: "#1a1b14"
+        },
+        {
+            name: "有何不可",
+            artist: "许嵩",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/%E8%AE%B8%E5%B5%A9-%E6%9C%89%E4%BD%95%E4%B8%8D%E5%8F%AF.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M000002KSDg90IaScI_1.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E8%AE%B8%E5%B5%A9-%E6%9C%89%E4%BD%95%E4%B8%8D%E5%8F%AF.lrc",
+            theme: "#edbe76"
+        },
+        {
+            name: "温泉",
+            artist: "许嵩 / 刘美麟",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/%E8%AE%B8%E5%B5%A9%26%E5%88%98%E7%BE%8E%E9%BA%9F-%E6%B8%A9%E6%B3%89.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M000000vKMVO0srdMF_1.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E8%AE%B8%E5%B5%A9%26%E5%88%98%E7%BE%8E%E9%BA%9F-%E6%B8%A9%E6%B3%89.lrc",
+            theme: "#e3b58b"
+        },
+        {
+            name: "素颜",
+            artist: "许嵩 / 何曼婷",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/%E8%AE%B8%E5%B5%A9%26%E4%BD%95%E6%9B%BC%E5%A9%B7-%E7%B4%A0%E9%A2%9C.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M0000035f8nw11cjkf_1.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E8%AE%B8%E5%B5%A9%26%E4%BD%95%E6%9B%BC%E5%A9%B7-%E7%B4%A0%E9%A2%9C.lrc",
+            theme: "#622931"
+        },
+        {
+            name: "打上花火",
+            artist: "米津玄师 / daoko",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%88%26daoko-%E6%89%93%E4%B8%8A%E8%8A%B1%E7%81%AB.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M000002rLPlR0CXaWS_3.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%88%26daoko-%E6%89%93%E4%B8%8A%E8%8A%B1%E7%81%AB.lrc",
+            theme: "#ed1306"
+        },
+        {
+            name: "Lemon",
+            artist: "米津玄师",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%88-Lemon.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M000000nPoD43Dybcc_1.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%88-Lemon.lrc",
+            theme: "#20778f"
+        },
+        {
+            name: "LOSER",
+            artist: "米津玄师",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%88-LOSER.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M000002kl9Hs4WaKFO_3.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%88-LOSER.lrc",
+            theme: "#13141d"
+        },
+        {
             name: "Numb",
             artist: "Linkin Park",
             url: "https://drive.imsyy.top/api?path=%2F%E9%9F%B3%E4%B9%90%2F%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8%2FLinkin%20Park-Numb.mp3&raw=true",
@@ -304,36 +368,28 @@ const ap = new APlayer({
             theme: "#270408"
         },
         {
-            name: "All About Us",
-            artist: "He Is We / Owl City",
-            url: "https://drive.imsyy.top/api?path=%2F%E9%9F%B3%E4%B9%90%2F%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8%2FHe%20Is%20We%26Owl%20City-All%20About%20Us.mp3&raw=true",
-            cover: "https://y.gtimg.cn/music/photo_new/T002R300x300M000004b2Efp2dEWlo_1.jpg?max_age=2592000",
-            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2FHe%20Is%20We%26Owl%20City-All%20About%20Us.lrc",
-            theme: "#ee5a75"
+            name: "My Love",
+            artist: "Westlife",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/Westlife-My%20Love.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M000000vux3D1vZI0u_1.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2FWestlife-My%20Love.lrc",
+            theme: "#e88ba6"
         },
         {
-            name: "You're Not Alone",
-            artist: "Owl City / Britt Nicole",
-            url: "https://drive.imsyy.top/api?path=%2F%E9%9F%B3%E4%B9%90%2F%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8%2FOwl%20City%26Britt%20Nicole-You're%20Not%20Alone.mp3&raw=true",
-            cover: "https://y.gtimg.cn/music/photo_new/T002R300x300M000002ke9Cq3KeWYj_1.jpg?max_age=2592000",
-            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2FOwl%20City%26Britt%20Nicole-You're%20Not%20Alone.lrc",
-            theme: "#eff8fa"
+            name: "Victory",
+            artist: "Two Steps From Hell / Thomas Bergersen",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/Two%20Steps%20From%20Hell%26Thomas%20Bergersen-Victory.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M000002hKKCC1LSc8y_2.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2FTwo%20Steps%20From%20Hell%26Thomas%20Bergersen-Victory.lrc",
+            theme: "#0a0c1b"
         },
         {
-            name: "Good Time",
-            artist: "Owl City / Carly Rae Jepsen",
-            url: "https://drive.imsyy.top/api?path=%2F%E9%9F%B3%E4%B9%90%2F%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8%2FOwl%20City%26Carly%20Rae%20Jepsen-Good%20Time.mp3&raw=true",
-            cover: "https://y.gtimg.cn/music/photo_new/T002R300x300M000001mat8F3tAzsu_1.jpg?max_age=2592000",
-            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2FOwl%20City%26Carly%20Rae%20Jepsen-Good%20Time.lrc",
-            theme: "#dd111c"
-        },
-        {
-            name: "Unbelievable",
-            artist: "Owl City / Hanson",
-            url: "https://drive.imsyy.top/api?path=%2F%E9%9F%B3%E4%B9%90%2F%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8%2FOwl%20City%26Hanson-Unbelievable.mp3&raw=true",
-            cover: "https://y.gtimg.cn/music/photo_new/T002R300x300M000004O8zk80vaqfl_1.jpg?max_age=2592000",
-            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2FOwl%20City%26Hanson-Unbelievable.lrc",
-            theme: "#de7227"
+            name: "Star Sky",
+            artist: "Two Steps From Hell / Thomas Bergersen",
+            url: "https://drive.imsyy.top/api?path=/%E9%9F%B3%E4%B9%90/%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8/Two%20Steps%20From%20Hell%26Thomas%20Bergersen-Star%20Sky.mp3&raw=true",
+            cover: "https://y.qq.com/music/photo_new/T002R300x300M000002hKKCC1LSc8y_2.jpg?max_age=2592000",
+            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2FTwo%20Steps%20From%20Hell%26Thomas%20Bergersen-Star%20Sky.lrc",
+            theme: "#0a0c1b"
         },
         {
             name: "Fireflies",
@@ -344,28 +400,12 @@ const ap = new APlayer({
             theme: "#33658d"
         },
         {
-            name: "Gold",
-            artist: "Owl City",
-            url: "https://drive.imsyy.top/api?path=%2F%E9%9F%B3%E4%B9%90%2F%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8%2FOwl%20City-Gold.mp3&raw=true",
-            cover: "https://y.gtimg.cn/music/photo_new/T002R300x300M000001Y76KO1Vpg7b_1.jpg?max_age=2592000",
-            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2FOwl%20City-Gold.lrc",
-            theme: "#94c1de"
-        },
-        {
             name: "The Saltwater Room",
             artist: "Owl City",
             url: "https://drive.imsyy.top/api?path=%2F%E9%9F%B3%E4%B9%90%2F%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8%2FOwl%20City-The%20Saltwater%20Room.mp3&raw=true",
             cover: "https://y.gtimg.cn/music/photo_new/T002R300x300M000002FEF7L03Cv7T_1.jpg?max_age=2592000",
             lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2FOwl%20City-The%20Saltwater%20Room.lrc",
             theme: "#33658d"
-        },
-        {
-            name: "When Can I See You Again？",
-            artist: "Owl City",
-            url: "https://drive.imsyy.top/api?path=%2F%E9%9F%B3%E4%B9%90%2F%E6%96%87%E4%BB%B6%E5%BC%95%E7%94%A8%2FOwl%20City-When%20Can%20I%20See%20You%20Again%EF%BC%9F.mp3&raw=true",
-            cover: "https://y.gtimg.cn/music/photo_new/T002R300x300M0000034UcqG3UwYdy_1.jpg?max_age=2592000",
-            lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2FOwl%20City-When%20Can%20I%20See%20You%20Again%EF%BC%9F.lrc",
-            theme: "#0e1c3a"
         }
     ]
 });
