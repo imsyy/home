1eab49d8bb94d268d2ab2d1ec5b442082b964cbb
底层用户 <ytqz104ban@126.com>
2022-06-30 11:58:43 +0800

    修改鼠标样式 #7

--- js/main.js
+++ js/main.js
@@ -23,6 +23,23 @@ iziToast.settings({
     iconColor: '#efefef',
 });
 
+/* 鼠标样式 */
+const body = document.querySelector("body");
+const element = document.getElementById("g-pointer-1");
+const element2 = document.getElementById("g-pointer-2");
+const halfAlementWidth = element.offsetWidth / 2;
+const halfAlementWidth2 = element2.offsetWidth / 2;
+
+function setPosition(x, y) {
+    element2.style.transform = `translate(${x - halfAlementWidth2 + 1}px, ${y - halfAlementWidth2 + 1}px)`;
+}
+
+body.addEventListener('mousemove', (e) => {
+    window.requestAnimationFrame(function () {
+        setPosition(e.clientX, e.clientY);
+    });
+});
+
 //加载完成后执行
 window.addEventListener('load', function () {
 
