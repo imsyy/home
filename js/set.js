9da5e5004081768c08dfbb82817a95635d82acb9
底层用户 <one@imsyy.top>
2022-12-10 15:17:24 +0800

    版本封存，即将使用 Vue 重构

--- js/set.js
+++ js/set.js
@@ -5,58 +5,6 @@ GitHub：https://github.com/imsyy/home
 版权所有，请勿删除
 */
 
-/* 自定义配置 */
-/* 尚未完善 */
-$(function () {
-    let url = "../setting.json"
-    $.getJSON(
-        url,
-        function (data) {
-            /* 页头数据 */
-            $('title').text(data.title);
-            $('#loading-title').html(data.title);
-            $("meta[name='description']").attr('content', data.description);
-            $("meta[name='keywords']").attr('content', data.keywords);
-            $("meta[name='author']").attr('content', data.author);
-            /* 基础信息 */
-            $("#logo-img").attr("src", data.logo_img);
-            $('#logo-text-1').html(data.logo_text_1);
-            $('#logo-text-2').html("." + data.logo_text_2);
-            $('#logo-title-other').html(data.logo_text_1);
-            $('#logo-title-other-small').html("." + data.logo_text_2);
-            $('#logo-text-small').html(data.logo_text_1 + "." + data.logo_text_2);
-            /* 社交链接 */
-            $('#github').attr('href', "https://github.com/" + data.github);
-            $('#qq').attr('href', "https://wpa.qq.com/msgrd?v=3&uin=" + data.qq + "&site=qq&menu=yes");
-            $('#email').attr('href', "mailto:" + data.email);
-            $('#bilibili').attr('href', "https://space.bilibili.com/" + data.bilibili);
-            $('#telegram').attr('href', "https://t.me/" + data.telegram);
-            /* 快捷链接 */
-            $('#link-url-1').attr('href', data.link_1[0]);
-            $('#link-icon-1').attr('class', data.link_1[1]);
-            $('#link-name-1').html(data.link_1[2]);
-            $('#link-url-2').attr('href', data.link_2[0]);
-            $('#link-icon-2').attr('class', data.link_2[1]);
-            $('#link-name-2').html(data.link_2[2]);
-            $('#link-url-3').attr('href', data.link_3[0]);
-            $('#link-icon-3').attr('class', data.link_3[1]);
-            $('#link-name-3').html(data.link_3[2]);
-            $('#link-url-4').attr('href', data.link_4[0]);
-            $('#link-icon-4').attr('class', data.link_4[1]);
-            $('#link-name-4').html(data.link_4[2]);
-            $('#link-url-5').attr('href', data.link_5[0]);
-            $('#link-icon-5').attr('class', data.link_5[1]);
-            $('#link-name-5').html(data.link_5[2]);
-            $('#link-url-6').attr('href', data.link_6[0]);
-            $('#link-icon-6').attr('class', data.link_6[1]);
-            $('#link-name-6').html(data.link_6[2]);
-            //页脚版权
-            $('#power-text').html(data.Copyright_text);
-            $('#beian').html("&amp;&nbsp;" + data.beian);
-        }
-    )
-});
-
 // 背景图片 Cookies 
 function setBgImg(bg_img) {
     if (bg_img) {
