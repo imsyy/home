5e98255888c7bc570bbc381d00d52462a376ebe0
土豆沙锅锅 <42053563+H539251932@users.noreply.github.com>
2023-02-04 17:15:03 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -319,7 +319,7 @@ $('#switchmore').on('click', function () {
     } else {
         $('#container').attr('class', 'container');
         $("#change").html("Hello&nbsp;World&nbsp;!");
-        $("#change1").html("一个建立于 21 世纪的小站，存活于互联网的边缘");
+        $("#change1").html("我在你身边扮演的角色太模糊了");
     }
 });
 
