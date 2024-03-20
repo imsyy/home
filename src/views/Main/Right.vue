36a98d684b692a65792f0742dc6bf2aa1fa2daef
woshinidad88 <62332284+woshinidad88@users.noreply.github.com>
2024-03-20 11:27:05 +0800

    Update Right.vue

--- src/views/Main/Right.vue
+++ src/views/Main/Right.vue
@@ -21,7 +21,7 @@ const store = mainStore();
 // 站点链接
 const siteUrl = computed(() => {
   const url = import.meta.env.VITE_SITE_URL;
-  if (!url) return "imsyy.top".split(".");
+  if (!url) return "1.010909.xyz".split(".");
   // 判断协议前缀
   if (url.startsWith("http://") || url.startsWith("https://")) {
     const urlFormat = url.replace(/^(https?:\/\/)/, "");
