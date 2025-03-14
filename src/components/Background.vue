ed7a64c6a1c174b19e1bd11e2ff66c9c9d054b07
hwxlikemi <159146868+hwxlikemi@users.noreply.github.com>
2025-03-14 20:04:13 +0800

    Update Background.vue

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -34,7 +34,7 @@ const emit = defineEmits(["loadComplete"]);
 
 // 壁纸随机数
 // 请依据文件夹内的图片个数修改 Math.random() 后面的第一个数字
-const bgRandom = Math.floor(Math.random() * 11 + 1);
+const bgRandom = Math.floor(Math.random() * 12 + 1);
 
 // 更换壁纸链接
 const changeBg = (type) => {
